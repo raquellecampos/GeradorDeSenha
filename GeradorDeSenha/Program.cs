eb873658b1ec2fe87b1e8fb8b31712f1cb058719
raquellecampos <raquellecampos12@gmail.com>
2021-08-16 00:16:36 -0400

    Adicionar arquivos de projeto.

---
+++ GeradorDeSenha/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade = 10;
+
+            string caracteresPossiveis = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ" + 
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ".ToLower() +
+                "0123456789" + "%&#@!";
+
+            Random sorteio = new Random();
+            int numeroSorteado = 0;
+            StringBuilder password = new StringBuilder();
+
+            for (int t = 0; t < quantidade; t++)
+            {
+                numeroSorteado = sorteio.Next(0, caracteresPossiveis.Length - 1); //sorteio.Next = fazer sorteio  / Length tamanho total
+                password.Append(caracteresPossiveis[numeroSorteado]);
+            }
+
+            Console.WriteLine(password);
+            Console.ReadKey();
+           
+
+        }
+    }
+}
---
+++ GeradorDeSenha/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("GeradorDeSenha")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeradorDeSenha")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d60e64b3-a848-4fdc-9d12-608aff68a3a0")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
